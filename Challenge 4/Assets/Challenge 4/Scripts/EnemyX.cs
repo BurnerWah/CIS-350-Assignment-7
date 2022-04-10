74760a8ba26e9700f88d57adbef9456d2f46d6d6
Jaden <jpleasants@mail.bradley.edu>
2022-04-10 18:05:21 -0500

    Base requirements

--- Challenge 4/Assets/Challenge 4/Scripts/EnemyX.cs
+++ Challenge 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -1,37 +1,36 @@
-﻿using System.Collections;
+﻿/*
+ * Jaden Pleasants
+ * Assignment 7
+ * Handles enemy behaviour
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyX : MonoBehaviour
-{
+public class EnemyX : MonoBehaviour {
     public float speed;
     private Rigidbody enemyRb;
     private GameObject playerGoal;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("/Goals/Player Goal");
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         // Set enemy direction towards player goal and move there
         Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
 
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
+    private void OnCollisionEnter(Collision other) {
         // If enemy collides with either goal, destroy it
-        if (other.gameObject.name == "Enemy Goal")
-        {
+        if (other.gameObject.name == "Enemy Goal") {
             Destroy(gameObject);
-        } 
-        else if (other.gameObject.name == "Player Goal")
-        {
+        } else if (other.gameObject.name == "Player Goal") {
             Destroy(gameObject);
         }
 
--- Challenge 4/Assets/Challenge 4/Scripts/RotateCameraX.cs
+++ Challenge 4/Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections;
+﻿/*
+ * Jaden Pleasants
+ * Assignment 7
+ * Rotates the camera
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateCameraX : MonoBehaviour
-{
+public class RotateCameraX : MonoBehaviour {
     private float speed = 200;
     public GameObject player;
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
 
-        transform.position = player.transform.position; // Move focal point with player
+        // Move focal point with player
+        transform.position = player.transform.position;
 
     }
 }
